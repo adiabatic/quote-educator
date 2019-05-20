fe869202027b31c60ec647ba0960e23bf53d4129
Nathan Galt <adiabatic@users.noreply.github.com>
2019-05-19 23:48:03 -0700

    Make first test work

--- main.go
+++ main.go
@@ -1,10 +1,90 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
+// EducateString is a convenience function for running Educate on strings.
+func EducateString(s string) (string, error) {
+	sr := strings.NewReader(s)
+	out := &strings.Builder{}
+
+	_, err := Educate(out, sr)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return out.String(), nil
+}
+
+// Educate curls quotes from in and writes them to out.
+//
+// Blindly copies the interface of io.Copy.
+func Educate(out io.Writer, in io.Reader) (written int64, err error) {
+
+	nextDoubleQuoteShouldBeOpening := true
+	nextSingleQuoteShouldBeOpening := true
+
+	inBuf := bufio.NewReader(in)
+	outBuf := bufio.NewWriter(out)
+	defer func() {
+		flushErr := outBuf.Flush()
+		if flushErr != nil {
+			log.Println(err)
+		}
+	}()
+
+	// defer outBuf.Flush()?
+
+	var r rune
+	var N int
+
+	for {
+		r = 0
+		N = 0
+
+		r, N, err = inBuf.ReadRune()
+		if err != nil {
+			return written, err
+		}
+		if r == unicode.ReplacementChar { // U+FFFD
+			return written, err
+		}
+
+		N = 0
+
+		// NB: this is all kinds of broken and handles lots of things wrongly
+		if r == '"' {
+			if nextDoubleQuoteShouldBeOpening {
+				r = '“'
+			} else {
+				r = '”'
+			}
+			nextDoubleQuoteShouldBeOpening = !nextDoubleQuoteShouldBeOpening
+		} else if r == '\'' {
+			if nextSingleQuoteShouldBeOpening {
+				r = '‘'
+			} else {
+				r = '’'
+			}
+			nextSingleQuoteShouldBeOpening = !nextSingleQuoteShouldBeOpening
+		}
+
+		// end transformation
+
+		N, err = outBuf.WriteRune(r)
+		if err != nil {
+			return written + int64(N), err
+		}
+		written += int64(N)
+	}
+}
+
 func main() {
-	io.Copy(os.Stdout, os.Stdin)
+	Educate(os.Stdout, os.Stdin)
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,30 @@
 package main_test
+
+import (
+	"testing"
+
+	quotes "github.com/adiabatic/quote-educator"
+)
+
+type Row struct {
+	In   string
+	Want string
+}
+
+func TestEverything(t *testing.T) {
+	rows := []Row{
+		{"hello", "hello"},
+	}
+
+	for _, row := range rows {
+		t.Run(row.In, func(t *testing.T) {
+			got, err := quotes.EducateString(row.In)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != row.Want {
+				t.Errorf("Expected «%s». got: «%s»", row.Want, got)
+			}
+		})
+	}
+}
